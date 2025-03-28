34701c4a17e38f1038f7177376bfef22641405fa
Wichi <sebastianbarbaro82@gmail.com>
2025-03-28 07:26:00 -0400

    Actualizar main-menugp.js

--- plugins/main-menugp.js
+++ plugins/main-menugp.js
@@ -24,7 +24,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname, args, command}) => {
 let tags = {
 'grupo': 'Menu Para Admins',
 }
-let img = 'https://qu.ax/TDAld.jpg'
+let img = 'https://qu.ax/iAJWO.jpg'
 
   try {
           // DEFAULT MENU
