2829a11908ceeab011d8e5e1a9ae2a92e0cb95f3
Wichi <sebastianbarbaro82@gmail.com>
2025-03-28 09:15:04 -0400

    Actualizar buscador-tiktoksearch.js

--- plugins/buscador-tiktoksearch.js
+++ plugins/buscador-tiktoksearch.js
@@ -8,7 +8,7 @@ import fetch from 'node-fetch';
 
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 
-if (!text) return m.reply(`🍭 Ingrese Un Texto Para Buscarlo En Tiktok\n> *Ejemplo:* ${usedPrefix + command} Crow Edits`);
+if (!text) return m.reply(`🍭 Ingrese Un Texto Para Buscarlo En Tiktok\n> *Ejemplo:* ${usedPrefix + command} Hayabu Edits`);
 
 try {
 let api = `https://delirius-apiofc.vercel.app/search/tiktoksearch?query=${text}`;
