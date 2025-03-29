43f801c73acbf5a80c6b176e9cf8cb97fb3187f1
Wichi <sebastianbarbaro82@gmail.com>
2025-03-28 21:51:04 -0400

    Actualizar config.js

--- config.js
+++ config.js
@@ -12,7 +12,8 @@ import moment from 'moment-timezone'
 global.owner = [
    ['51941658192', 'Creador Hayusa', true],
    ['584146277368', 'Barbpza', true],
-   ['584245610338', 'Haya',true],
+   ['5804245610338', 'jose', true],
+   
 ]
    
 global.creadorbot = [
